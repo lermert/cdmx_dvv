138651b7227e7fe12bf1df7360453b1c73370b7f
Laura <laura.ermert@posteo.net>
2022-03-17 17:18:03 -0700

    readme !

---
+++ README.md
@@ -0,0 +1,12 @@
+### Data analysis for dv/v measurements for manuscript (title tbd)
+
+This set of scripts can be used to run Monte Carlo inversions with the emcee module (https://github.com/dfm/emcee) so as to obtain optimal-fit parameters for modeling velocity change in Mexico City. 
+
+To reproduce our inversions:
+1. Head to zenodo to obtain the data files (to be added), which contain time stamps, dv/v measurements and various other input.
+2. Select the relevant input yaml file from the reproducibility folder and edit it to your liking.
+3. Run: python m.py \<path-to-input.yml\>
+
+
+Brief provenance of the data in the zenodo repository: Correlations were obtained with ants https://github.com/lermert/ants_2, using the input files (with minor adaptations for different data sources, instruments etc) in reproducibility folder (to be added). Clustering, stacking and stretching measurements were performed using the ruido module https://github.com/lermert/ruido, again with input files as in reproducibility folder. 
+
--- velocity_models.py
+++ velocity_models.py
@@ -9,15 +9,15 @@
 """
 #--------------------------------------------------------------------------------------------------
 #- Ciudad de Mexico, approximate model for the sediment & upper part of volcanics
-#- based on info from Perez Cruz 1988, Singh et al. 1995, Singh et al. 1997, Shapiro et al. 2002
-#- plus educated guesses to fill the gaps between ~ 100 m and the sediment bottom
+#- based on info from Perez Cruz 1988, Singh et al. 1995, Singh et al. 1997,
+#-  Shapiro et al. 2002, Cruz Atienza et al, 2018
 #- model names refer to locations of the RSVM seismic network and the Geoscope station G.UNM
 #  Additional data from:
 # - Aquitard thickness used for lake (> 0 m) or hill (=0 m) designation, (note this is for the
 #   purpose of this study only, and not consistent with geotechnical classification),
 #   and to upper_z value of clay thickness: Solano Rojas et al., 2015
 #   http://www.scielo.org.mx/scielo.php?script=sci_arttext&pid=S1405-33222015000200011
-# - Approximate sediment depth: Information from well logs, Dario Solano Rojas & Diana Morales, pers. comm.
+# - Hard sites, intermediate sites, soft sites: Quintanar et al. 2018
 # - Approximate shear wave velocity, shallow: Singh et al., 1997
 # - Approximate shear wave velocity, deeper: Shapiro et al. 2002
 # - Approximate P wave velocity and density: Singh et al., 1995, Singh et al. 1997, Perez Cruz 1988
@@ -31,61 +31,57 @@
 def model_cdmx_discrete(z, model, output="v_rho_q", z_is_radius=False):
     
     # BEDROCK
-    if model in ["cdmx_ipvm", "cdmx_aovm", "cdmx_gmvm", "cdmx_cjvm",
+    if model in ["cdmx_aovm", "cdmx_cjvm", "cdmx_gmvm", "cdmx_ipvm", "cdmx_mhvm",
     "cdmx_mpvm", "cdmx_mzvm", "cdmx_ptvm", "cdmx_tlvm", "ESTA", "cdmx_ESTA",
     "cdmx_mcvm"]:
-        sed_depth=0.
-        zone="hill"
+        clay_depth = 0.0
+        sed_depth = 0.
+        site_type = "hard"
 
     # SEDIMENT (NO / VERY LITTLE CLAY)
-    elif model == "cdmx_xcvm":
-        zone = "hill"
-        upper_z = 0.0
-        sed_depth = 15.0
-    elif model in ["cdmx_thvm", "TEPE", "cdmx_TEPE",]:
-        zone="hill"
-        sed_depth = 50.0
-    elif model in ["cdmx_unm", "cdmx_mhvm"]:
+    elif model in ["cdmx_xcvm", "cdmx_unm", "cdmx_UNM", "UNM", "unm", "TEPE", "cdmx_TEPE", "cdmx_thvm"]:
+        clay_depth = 0.0
         sed_depth = 100.0
-        zone = "hill"
+        site_type = "intermediate"
 
     # SEDIMENT (INCL. CLAY)
     elif model in ["cdmx_bjvm",  "cdmx_test"]:
-        sed_depth = 200.
-        zone = "lake"
-        upper_z = 25
-    elif model == "cdmx_icvm":
-        sed_depth = 400.
-        zone = "lake"
-        upper_z = 75.
+        clay_depth = 25
+        sed_depth = 100.
+        site_type = "intermediate"
     elif model == "cdmx_covm":
-        sed_depth = 300.
-        zone = "lake"
-        upper_z = 40.
-    elif model == "cdmx_ctvm":
-        zone = "lake"
-        upper_z = 55
-        sed_depth = 300.
-    elif model == "cdmx_vrvm":
-        zone = "lake"
-        upper_z = 50.0
-        sed_depth = 300.
+        clay_depth = 40.0
+        sed_depth = 100.
+        site_type = "intermediate"
     elif model == "cdmx_apvm":
-        zone = "lake"
-        upper_z = 20.0
-        sed_depth = 200.0
+        clay_depth = 20.0
+        sed_depth = 100.0
+        site_type = "intermediate"
+
+    elif model == "cdmx_ctvm":
+        clay_depth = 55
+        sed_depth = 300.
+        site_type = "soft"
+    elif model == "cdmx_icvm":
+        clay_depth = 75.
+        sed_depth = 300.
+        site_type = "soft"
+    elif model == "cdmx_vrvm":
+        clay_depth = 50.0
+        sed_depth = 300.
+        site_type = "soft"
     elif model in ["MULU", "cdmx_MULU"]:
-        zone = "lake"
-        upper_z = 30
+        clay_depth = 30
         sed_depth = 300
+        site_type = "soft"
     elif model in ["CIRE", "cdmx_CIRE"]:
-        zone = "lake"
-        upper_z = 35
+        clay_depth = 35
         sed_depth = 300
+        site_type = "soft"
     elif model in ["MIXC", "cdmx_MIXC"]:
-        zone = "lake"
-        upper_z = 10
-        sed_depth = 300
+        clay_depth = 10
+        sed_depth = 100
+        site_type = "intermediate"
     else:
         raise ValueError("Unknown model {}".format(model))
 
@@ -93,47 +89,37 @@ def model_cdmx_discrete(z, model, output="v_rho_q", z_is_radius=False):
     if z_is_radius:
         z = 6371000.0 - z # with respect to Earth radius
 
-    if zone == "lake":
-        upper_z = upper_z
-        upper_vs = 50.
-        upper_vp = 800.
-        upper_rho = 1250.
-        fluid_volume_fraction = 0.6 # using porosity as proxy
-        # porosity from Ortega Guerrero & Farvolden, 1989
-
-    if zone == "hill":
-        upper_z = 0.
-        upper_vs = 400.
-        upper_vp = 2500.
-        upper_rho = 2000.0
-        fluid_volume_fraction = 0.2  # using porosity as proxy
-        # porosity from Ortega Guerrero & Farvolden, 1989
     if z < 0:
         z = 0.
-    if z < upper_z:
-        vs = upper_vs
-        vp = upper_vp
-        rho = upper_rho
+    if z < clay_depth:
+        vs = 50.0
+        vp = 800.0
+        rho = 1250.0
         qs = 60.
         qp = 120.
-    elif z >= upper_z and z < sed_depth / 2:
+        fluid_volume_fraction = 0.6 # using porosity as proxy
+        # porosity from Ortega Guerrero & Farvolden, 1989
+    elif z >= clay_depth and z < sed_depth / 2:
         vs = 400.
         vp = 2500.
         rho = 2000.
         qs = 115.
         qp = 230.
+        fluid_volume_fraction = 0.2  # using porosity as proxy
     elif z >= sed_depth / 2 and z < sed_depth:
         vs = 800.
         vp = 2500.
         rho = 2000.
         qs = 115.
         qp = 230
+        fluid_volume_fraction = 0.2  # using porosity as proxy
     elif z >= sed_depth:
         vs = 1050.
         vp = 2600.
         rho = 2000.
         qs = 115.
         qp = 230.
+        fluid_volume_fraction = 0.2  # using porosity as proxy
     elif z >= sed_depth + 1000.0:
     # harder bedrock (see cross-section in Singh 95)
         vs = 2100.
@@ -141,13 +127,7 @@ def model_cdmx_discrete(z, model, output="v_rho_q", z_is_radius=False):
         rho = 2000.0
         qs = 115.
         qp = 230.
-    if zone == "lake":
-        if z >= upper_z and z < sed_depth / 4:
-            vs = 200.
-            vp = 1550.
-            rho = 2000.
-            qs = 60.
-            qp = 120.
+        fluid_volume_fraction = 0.2  # using porosity as proxy
 
     if model == "cdmx_test":
         # test the model with less extreme Poisson ratio
@@ -156,14 +136,15 @@ def model_cdmx_discrete(z, model, output="v_rho_q", z_is_radius=False):
     if output == "poroelastic":
         k_s = 35.e9
         k_w  = 2.5e9 
-        poisson_ratio = (vp ** 2 - 2. * vs ** 2) / (2. * vp ** 2 - 2. * vs ** 2)
-        upper_nu = (upper_vp ** 2 - 2. * upper_vs ** 2) / (2. * upper_vp ** 2 - 2. * upper_vs ** 2)
-        mu = upper_vs ** 2 * upper_rho
-        lam = upper_vp ** 2 * upper_rho - 2 * mu
+        nu =  0.25
+        nu_u = (vp ** 2 - 2. * vs ** 2) / (2. * vp ** 2 - 2. * vs ** 2)
+        mu = vs ** 2 * rho
+        lam = vp ** 2 * rho - 2 * mu
         k = lam + 2. / 3. * mu  # bulk modulus
+
         B_clearyrice = (1 / k - 1 / k_s) / (fluid_volume_fraction * (1 / k_w - 1 / k_s) + (1 / k - 1 / k_s))
-        nu_u_clearyrice = (3. * upper_nu + B_clearyrice * (1 - 2. * upper_nu) * (1 - k / k_s)) / (3. - B_clearyrice * (1. - 2. * poisson_ratio) * (1. - k / k_s))
-        return(vs, vp, rho, qs, qp, poisson_ratio, B_clearyrice, nu_u_clearyrice)
+        # nu_u_clearyrice = (3. * upper_nu + B_clearyrice * (1 - 2. * upper_nu) * (1 - k / k_s)) / (3. - B_clearyrice * (1. - 2. * poisson_ratio) * (1. - k / k_s))
+        return(vs, vp, rho, qs, qp, nu, B_clearyrice, nu_u)
     elif output == "elastic":
         #- convert to elastic parameters -----------------------------------------
         eta = 1.0  # isotropic model
